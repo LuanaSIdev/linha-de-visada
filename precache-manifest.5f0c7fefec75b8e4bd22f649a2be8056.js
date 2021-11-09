678e5013dd1d410b258d0a9ceadd084ae985f24c
LuanaSIdev <luanafarias443@gmail.com>
2021-11-09 16:48:09 -0300

    Updates

---
+++ precache-manifest.5f0c7fefec75b8e4bd22f649a2be8056.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221f6637dadac96e2009a621fceae9b9",
+    "url": "/linha-de-visada/index.html"
+  },
+  {
+    "revision": "11d0c86a76d0b039c689",
+    "url": "/linha-de-visada/static/css/2.0f2214df.chunk.css"
+  },
+  {
+    "revision": "3a2676e8c11e48bbf533",
+    "url": "/linha-de-visada/static/css/main.fac80f52.chunk.css"
+  },
+  {
+    "revision": "11d0c86a76d0b039c689",
+    "url": "/linha-de-visada/static/js/2.c30e8f0c.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/linha-de-visada/static/js/2.c30e8f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2676e8c11e48bbf533",
+    "url": "/linha-de-visada/static/js/main.acf1f01c.chunk.js"
+  },
+  {
+    "revision": "0caedf2210bf17efeea0",
+    "url": "/linha-de-visada/static/js/runtime-main.248d4785.js"
+  },
+  {
+    "revision": "7dfee890f2376254786bbc75e0bd6646",
+    "url": "/linha-de-visada/static/media/antenna.7dfee890.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/linha-de-visada/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/linha-de-visada/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/linha-de-visada/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/linha-de-visada/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/linha-de-visada/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "802d33c4d4943897f223a0cd189a9417",
+    "url": "/linha-de-visada/static/media/reload.802d33c4.png"
+  }
+]);
